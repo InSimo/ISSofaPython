c06418d805a0aefd487e0b9deab274a7813427b3
François Jourdes <francois.jourdes@insimo.fr>
2018-04-30 10:11:27 +0200

    UPDATE: Bind BaseLink read method.
    (cherry picked from commit 4c7bb48724974142c847aed2ae6390f951c7070f)

--- module/BaseLinkBinding.cpp
+++ module/BaseLinkBinding.cpp
@@ -9,6 +9,8 @@
 #include <sofa/core/objectmodel/BaseData.h>
 #include <sofa/core/objectmodel/Base.h>
 
+
+
 namespace sofa
 {
 
@@ -17,12 +19,23 @@ namespace python
 
 using sofa::core::objectmodel::BaseLink;
 
+
+void baseLinkRead(BaseLink* link, const std::string& path)
+{
+    if (!link->read(path))
+    {
+        throw  std::invalid_argument(path.c_str());
+    }
+}
+
+
 void initBindingBaseLink(pybind11::module& m)
 {
     pybind11::class_<BaseLink>(m, "BaseLink")
         .def("setName", &BaseLink::setName)
         .def("getName", &BaseLink::getName)
         .def_property("name", &BaseLink::getName, &BaseLink::setName)
+        .def("read", [](BaseLink* link, const std::string& path) { baseLinkRead(link, path);  })
         .def("getSize", &BaseLink::getSize)
         .def("getLinkedPath", &BaseLink::getLinkedPath, pybind11::arg("index")=0)
         .def("getLinkedBase", &BaseLink::getLinkedBase, pybind11::arg("index")=0)
