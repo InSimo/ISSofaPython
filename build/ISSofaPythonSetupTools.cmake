29420151bf24a8fe01247be8756989694526d244
François Jourdes <francois.jourdes@insimo.fr>
2018-06-19 11:49:42 +0200

    [ISSofaPython] UPDATE: issofapython_add_package cmake function prevents custom targets from concurrently writing easy-install.pth
    In order to achieve that a dependency management between the different
    targets that write into that particular file has been created. The logic
    is to keep track in a global variable the name of the last target that
    writes into the easy-install.pth file.
    
    Ref T5453
    
    (cherry picked from commit 91a4b3e37fbb3a85af5b2c24178f6d97b471919d)

--- build/ISSofaPythonSetupTools.cmake
+++ build/ISSofaPythonSetupTools.cmake
@@ -5,31 +5,64 @@ include(CMakeParseArguments)
 # Add python packages described by a setuptools setup.py file
 # Underneath this method will create a cmake target which performs
 # an "editable" install of the packages described in the setup.py file
-# issofapython_add_package(<setup_py_file_path>)
+# issofapython_add_package( TARGET <target_name> 
+#                           SETUP_FILE <setup_py_file_path>
+#                         )
 #
 function(issofapython_add_package)
-    cmake_parse_arguments(ARG "" "" "" ${ARGN})
+    set(ARGV TARGET SETUP_FILE )
+    cmake_parse_arguments(ARG "" "" "${ARGV}" ${ARGN})
+    
     find_package(PythonInterp REQUIRED)
     if(NOT PYTHONINTERP_FOUND)
         message(FATAL_ERROR "Python interpreter could not be found.")
         return()
     endif()
-    set(PROJECT_PYTHON_SETUPTOOLSFILE ${ARG_UNPARSED_ARGUMENTS})
+    
+    if(${ARG_TARGET} STREQUAL "")
+        message(FATAL_ERROR "No target name specified.")
+    endif()
+    set(PROJECT_TARGET_PYHON_PACKAGE ${ARG_TARGET})
+    
+    set(PROJECT_PYTHON_SETUPTOOLSFILE ${ARG_SETUP_FILE})
     if(NOT EXISTS ${PROJECT_PYTHON_SETUPTOOLSFILE} )
-        message("No python package setup file provided. Returning")
+        message(FATAL_ERROR "No python package setup file provided.")
     endif()
     get_filename_component(PROJECT_PYTHON_SETUPTOOLS_DIR ${PROJECT_PYTHON_SETUPTOOLSFILE} DIRECTORY)
-    set(PROJECT_TARGET_PYHON_PACKAGE_UNINSTALL "${PROJECT_NAME}PythonPackageUninstall")
-    set(PROJECT_TARGET_PYHON_PACKAGE_INSTALL "${PROJECT_NAME}PythonPackageInstall")
+    
+    
+    get_property( LAST_PACKAGE GLOBAL PROPERTY ISSOFAPYTHON_LAST_ADDED_PACKAGE )
+    set(PROJECT_TARGET_PYTHON_PACKAGE_DEPENDS ${LAST_PACKAGE} )
 
-    set(PROJECT_TARGET_PYHON_SETUP ${PROJECT_NAME}PythonSetup)
-
-    add_custom_target(${PROJECT_TARGET_PYHON_SETUP} ALL)
-    add_custom_command( TARGET ${PROJECT_TARGET_PYHON_SETUP}
+    if(NOT PYTHON_PIP_DEVELOP_EASY_INSTALL_FILE )
+        execute_process(COMMAND ${PYTHON_EXECUTABLE} -m site --user-site 
+                        OUTPUT_VARIABLE PYTHON_USER_SITE_DIR 
+                        OUTPUT_STRIP_TRAILING_WHITESPACE )
+        set(PYTHON_PIP_DEVELOP_EASY_INSTALL_FILE "${PYTHON_USER_SITE_DIR}/easy-install.pth" CACHE "" INTERNAL)
+    endif()
+    
+    list( APPEND PROJECT_TARGET_PYTHON_PACKAGE_DEPENDS ${PYTHON_PIP_DEVELOP_EASY_INSTALL_FILE} )
+    list( APPEND PROJECT_TARGET_PYTHON_PACKAGE_DEPENDS ${PROJECT_PYTHON_SETUPTOOLSFILE} )
+    
+    add_custom_target(${PROJECT_TARGET_PYHON_PACKAGE} ALL
+        DEPENDS ${PROJECT_TARGET_PYTHON_PACKAGE_DEPENDS}
+    )
+    
+    # unfortunately add_custom_command does not support files that are both a dependency and an output of a command
+    # ( by maintining a write counter on the file for example ). It results in a cyclic dependency, hence the 
+    # intricate global variable ISSOFAPYTHON_LAST_ADDED_PACKAGE which keeps track of the last added target that 
+    # writes into the easy-install.pth file, so that it can be added as a dependency to prevent concurrent writes 
+    # on that file.
+    add_custom_command(
         PRE_BUILD
+        # OUTPUT ${PYTHON_PIP_DEVELOP_EASY_INSTALL_FILE}
+        TARGET ${PROJECT_TARGET_PYHON_PACKAGE}
         COMMAND ${PYTHON_EXECUTABLE} -m pip install --user --editable "./"
         WORKING_DIRECTORY ${PROJECT_PYTHON_SETUPTOOLS_DIR}
-        DEPENDS ${PROJECT_PYTHON_SETUPTOOLS_DIR}
+        DEPENDS ${PROJECT_PYTHON_SETUPTOOLSFILE} 
     )
-    set_target_properties(${PROJECT_TARGET_PYHON_SETUP} PROPERTIES FOLDER ${PROJECT_NAME}PythonTargets)
+    
+    set_property(GLOBAL PROPERTY ISSOFAPYTHON_LAST_ADDED_PACKAGE ${PROJECT_TARGET_PYHON_PACKAGE} )
 endfunction()
+
+
