512a3008e2206c2ed990bb55a69b7e575adc2127
Gauthier Zirnhelt <gauthier.zirnhelt@insimo.fr>
2018-08-01 15:49:40 +0200

    [ISSofaPython] actually remove double parsing
    (cherry picked from commit 89f9d3bc97345165a79bb3681c04075b063cac61)

--- tests/test_pythonsceneloader_sceneargument.py
+++ tests/test_pythonsceneloader_sceneargument.py
@@ -1,10 +1,10 @@
 # If called through runSofa with a scene option, the object added to the scene will be named after this option
 # If called directly with the python interpreter, the object added to scene will be named after the first command-line argument
 
-import ISSofaPython as Sofa
 import sys
+import ISSofaPython as Sofa
 
-objName = "rootObject"
+objName = "defaultName"
 
 
 def parseSceneArguments(args):
@@ -24,5 +24,4 @@ def createScene(root):
 
 
 if __name__ == "__main__":
-    parseSceneArguments(sys.argv[1:])
     createScene(Sofa.GNode())
