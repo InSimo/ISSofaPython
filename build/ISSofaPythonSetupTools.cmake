0273e734e72ef0fe57449daa92bbb2d8b87c860c
François Jourdes <francois.jourdes@insimo.fr>
2018-09-24 18:08:58 +0200

    [ISSofaPython] Remove support of install of python packages during build. The default behavior is to install python packages at cmake time.
    (cherry picked from commit e34cecb1a7f81462c2e3b59b6734928753b46057)

--- build/ISSofaPythonSetupTools.cmake
+++ build/ISSofaPythonSetupTools.cmake
@@ -16,10 +16,6 @@ cmake_dependent_option(ISSOFAPYTHON_INSTALL_USER "If ISSOFAPYTHON_USE_VIRTUALENV
 set(ISSOFAPYTHON_INSTALL_PREFIX_DIR ${CMAKE_BINARY_DIR} CACHE PATH "If ISSOFAPYTHON_USE_VIRTUALENV is OFF and ISSOFAPYTHON_INSTALL_USER is OFF: directory to use for python installation (default to build directory)")
 set(ISSOFAPYTHON_PIP_INSTALL_OPTIONS "--editable" CACHE STRING "Options given to pip install commands")
 
-option(ISSOFAPYTHON_INSTALL_DURING_CMAKE "Enable installation of python packages during configuration" OFF)
-option(ISSOFAPYTHON_INSTALL_DURING_BUILD "Enable installation of python packages during build" ON)
-#option(ISSOFAPYTHON_INSTALL_DURING_INSTALL "Enable installation of python packages during install" OFF)
-
 if(ISSOFAPYTHON_USE_VIRTUALENV)
     get_filename_component(PYTHON_EXECUTABLE_RELPATH ${PYTHON_EXECUTABLE} NAME)
     if(WIN32)
@@ -49,64 +45,22 @@ elseif(ISSOFAPYTHON_INSTALL_USER)
 endif()
 
 # Add python packages described by a setuptools setup.py file
-# Underneath this method will create a cmake target which performs
-# an "editable" install of the packages described in the setup.py file
-# issofapython_add_package( TARGET <target_name> 
-#                           SETUP_FILE <setup_py_file_path>
-#                         )
+# Underneath this method will launch the pip install command with the
+# appropriate arguments, depending on wether or not a virtualenv is
+# installed.
+# issofapython_add_package( <setup_py_file_path> )
 #
 function(issofapython_add_package)
-    set(ARGV TARGET SETUP_FILE )
-    cmake_parse_arguments(ARG "" "" "${ARGV}" ${ARGN})
+    cmake_parse_arguments(ARG "" "" "" ${ARGN})
     
-    if(${ARG_TARGET} STREQUAL "")
-        message(FATAL_ERROR "No target name specified.")
-    endif()
-    set(PROJECT_TARGET_PYTHON_PACKAGE ${ARG_TARGET})
-    
-    set(PROJECT_PYTHON_SETUPTOOLSFILE ${ARG_SETUP_FILE})
+    set(PROJECT_PYTHON_SETUPTOOLSFILE ${ARG_UNPARSED_ARGUMENTS})
     if(NOT EXISTS ${PROJECT_PYTHON_SETUPTOOLSFILE} )
         message(FATAL_ERROR "No python package setup file provided.")
     endif()
     get_filename_component(PROJECT_PYTHON_SETUPTOOLS_DIR ${PROJECT_PYTHON_SETUPTOOLSFILE} DIRECTORY)
     
-    if(ISSOFAPYTHON_INSTALL_DURING_CMAKE)
-        execute_process(
-            COMMAND ${ISSOFAPYTHON_EXECUTABLE} -m pip install ${ISSOFAPYTHON_PIP_INSTALL_OPTIONS_ALL} "./"
-            WORKING_DIRECTORY ${PROJECT_PYTHON_SETUPTOOLS_DIR}
-        )
-    endif()
-    
-    if(ISSOFAPYTHON_INSTALL_DURING_BUILD)
-        get_property( LAST_PACKAGE GLOBAL PROPERTY ISSOFAPYTHON_LAST_ADDED_PACKAGE )
-        set(PROJECT_TARGET_PYTHON_PACKAGE_DEPENDS ${LAST_PACKAGE} )
-        if(ISSOFAPYTHON_INSTALL_USER)
-            if (NOT EXISTS "${PYTHON_PIP_DEVELOP_EASY_INSTALL_FILE}")
-                file(WRITE "${PYTHON_PIP_DEVELOP_EASY_INSTALL_FILE}" "")
-            endif()
-            list( APPEND PROJECT_TARGET_PYTHON_PACKAGE_DEPENDS ${PYTHON_PIP_DEVELOP_EASY_INSTALL_FILE} )
-        endif()
-        list( APPEND PROJECT_TARGET_PYTHON_PACKAGE_DEPENDS ${PROJECT_PYTHON_SETUPTOOLSFILE} )
-
-        # unfortunately add_custom_command does not support files that are both a dependency and an output of a command
-        # ( by maintaining a write counter on the file for example ). It results in a cyclic dependency, hence the
-        # intricate global variable ISSOFAPYTHON_LAST_ADDED_PACKAGE which keeps track of the last added target that
-        # writes into the easy-install.pth file, so that it can be added as a dependency to prevent concurrent writes
-        # on that file.
-        set(STAMP ${CMAKE_CURRENT_BINARY_DIR}/stamp-pip-${PROJECT_TARGET_PYTHON_PACKAGE}.txt )
-        add_custom_command(
-            PRE_BUILD
-            OUTPUT ${STAMP}
-            COMMAND ${ISSOFAPYTHON_EXECUTABLE} -m pip install ${ISSOFAPYTHON_PIP_INSTALL_OPTIONS_ALL} "./"
-            COMMAND ${CMAKE_COMMAND} -E touch ${STAMP}
-            WORKING_DIRECTORY ${PROJECT_PYTHON_SETUPTOOLS_DIR}
-            DEPENDS ${PROJECT_PYTHON_SETUPTOOLSFILE}
-        )
-
-        add_custom_target(${PROJECT_TARGET_PYTHON_PACKAGE} ALL
-            DEPENDS ${STAMP} ${PROJECT_TARGET_PYTHON_PACKAGE_DEPENDS}
-            )
-
-      set_property(GLOBAL PROPERTY ISSOFAPYTHON_LAST_ADDED_PACKAGE ${PROJECT_TARGET_PYTHON_PACKAGE} )
-    endif()
+    execute_process(
+        COMMAND ${ISSOFAPYTHON_EXECUTABLE} -m pip install ${ISSOFAPYTHON_PIP_INSTALL_OPTIONS_ALL} "./"
+        WORKING_DIRECTORY ${PROJECT_PYTHON_SETUPTOOLS_DIR}
+    )
 endfunction()
