d1621c874df347bf7c6446e939acdd39e04fb305
François Jourdes <francois.jourdes@insimo.fr>
2018-11-19 18:22:04 +0100

    [ISSofaPython] Add search method bindings to BaseContext to be able to retrieve objects in the scene graph from their type
    The code supersedes the `getObjects` binding method which was never used
    in practice.
    The semantic used for the methods is `search[All]Dir`,
    e.g `SearchAllUp`, `SearchLocal`.
    Search methods work with object types, described by a class name and an
    optional template name.
    It contrasts with the `getObject` method which relies on a path to
    retrieve an object instance, which requires by definition the
    knowledge of the name of the object we are looking for to succeed.
    
    Ref T6299
    
    (cherry picked from commit 64e5cf2a54900106bb67428572358df94a5770ba)

--- module/BaseContextBinding.cpp
+++ module/BaseContextBinding.cpp
@@ -2,6 +2,8 @@
 #include <sofa/core/objectmodel/BaseContext.h>
 #include <sofa/core/objectmodel/Base.h>
 #include <sofa/core/objectmodel/BaseObject.h>
+#include <sofa/core/ObjectFactory.h>
+#include <sofa/defaulttype/TemplatesAliases.h>
 #include <pybind11/stl.h>
 
 namespace sofa
@@ -14,23 +16,146 @@ using sofa::core::objectmodel::Base;
 using sofa::core::objectmodel::BaseContext;
 using sofa::core::objectmodel::BaseObject;
 using sofa::core::objectmodel::BaseClass;
+using sofa::core::ObjectFactory;
 
-std::vector< BaseObject::SPtr > getObjects(BaseContext* ctx, const BaseClass* classInfo, BaseContext::SearchDirection dir)
+std::vector< BaseObject::SPtr > getAllObjects(BaseContext* ctx, BaseContext::SearchDirection dir)
 {
-    std::vector< BaseObject::SPtr > all_objects;
-    std::vector< BaseObject::SPtr > objects;
+    std::vector< BaseObject::SPtr > allObjects;
+    ctx->get< BaseObject >(&allObjects, dir);
+    return  allObjects;
+}
 
-    ctx->get< BaseObject >(&all_objects, dir);
-
-    for (const auto& obj : all_objects)
+void appendMatchingObjects( std::vector< pybind11::object >& objects, 
+                            const std::vector< BaseObject::SPtr>& allObjects, 
+                            const BaseClass* classInfo)
+{
+    for (const auto& obj : allObjects)
     {
         if ( classInfo->dynamicCast(obj.get()) )
         {
-            objects.push_back(obj);
+            objects.push_back(bindDataAndLinks(obj));
+        }
+    }
+}
+
+std::vector< const BaseClass* > getObjectBaseClass(std::string className, std::string templateName)
+{
+    std::vector< const BaseClass* > objectClass;
+
+    const auto& registry = ObjectFactory::getInstance()->getRegistry();
+    templateName = sofa::defaulttype::TemplateAliases::resolveAlias(templateName);
+    auto itEntry = registry.find(className);
+    if (itEntry != registry.end())
+    {
+        ObjectFactory::ClassEntry::SPtr entry = itEntry->second;
+        if (!templateName.empty())
+        {
+            auto itCreator = entry->creatorMap.find(templateName);
+            if (itCreator != entry->creatorMap.end())
+            {
+                objectClass.push_back(itCreator->second->getClass());
+            }
+        }
+        else
+        {
+            for (auto kv : entry->creatorMap)
+            {
+                objectClass.push_back(kv.second->getClass());
+            }
         }
     }
 
-    return objects;
+    return objectClass;
+
+}
+
+std::vector< pybind11::object > searchObjects(BaseContext* ctx, BaseContext::SearchDirection dir, std::string className, std::string templateName)
+{
+    std::vector< BaseObject::SPtr > allObjects = getAllObjects(ctx, dir);
+    std::vector< pybind11::object > allBindedObjects;
+    if (className.empty())
+    {
+        for (const auto& obj : allObjects)
+        {
+            allBindedObjects.push_back(bindDataAndLinks(obj));
+        }
+        
+        return allBindedObjects;
+    }
+    else
+    {
+        std::vector< pybind11::object > objectsFound;
+        std::vector< const BaseClass* > objectBaseClass = getObjectBaseClass(className, templateName);
+        for (const BaseClass* baseClass : objectBaseClass)
+        {
+            appendMatchingObjects(objectsFound, allObjects, baseClass);
+        }
+
+        return objectsFound;
+    }
+}
+
+pybind11::object searchFirstObject(BaseContext* ctx, BaseContext::SearchDirection dir, std::string className, std::string templateName)
+{
+    std::vector< pybind11::object > objects = searchObjects(ctx, dir, className, templateName);
+    if (objects.empty())
+    {
+        return pybind11::object();
+    }
+    else
+    {
+        return objects[0];
+    }
+}
+
+std::vector< pybind11::object > searchAllUp(BaseContext* ctx, std::string className, std::string templateName)
+{
+    return searchObjects(ctx, BaseContext::SearchUp, className, templateName);
+}
+
+pybind11::object searchUp(BaseContext* ctx, std::string className, std::string templateName)
+{
+    return searchFirstObject(ctx, BaseContext::SearchUp, className, templateName);
+}
+
+std::vector< pybind11::object > searchAllLocal(BaseContext* ctx, std::string className, std::string templateName)
+{
+    return searchObjects(ctx, BaseContext::Local, className, templateName);
+}
+
+pybind11::object searchLocal(BaseContext* ctx, std::string className, std::string templateName)
+{
+    return searchFirstObject(ctx, BaseContext::Local, className, templateName);
+}
+
+std::vector< pybind11::object > searchAllParents(BaseContext* ctx, std::string className, std::string templateName)
+{
+    return searchObjects(ctx, BaseContext::SearchParents, className, templateName);
+}
+
+pybind11::object searchParents(BaseContext* ctx, std::string className, std::string templateName)
+{
+    return searchFirstObject(ctx, BaseContext::SearchParents, className, templateName);
+}
+
+std::vector< pybind11::object > searchAllDown(BaseContext* ctx, std::string className, std::string templateName)
+{
+    return searchObjects(ctx, BaseContext::SearchDown, className, templateName);
+}
+
+pybind11::object searchDown(BaseContext* ctx, std::string className, std::string templateName)
+{
+    return searchFirstObject(ctx, BaseContext::SearchDown, className, templateName);
+}
+
+std::vector< pybind11::object > searchAllRoot(BaseContext* ctx, std::string className, std::string templateName)
+{
+    return searchObjects(ctx, BaseContext::SearchRoot, className, templateName);
+}
+
+pybind11::object searchRoot(BaseContext* ctx, std::string className, std::string templateName)
+{
+    return searchFirstObject(ctx, BaseContext::SearchRoot, className, templateName);
 }
 
 pybind11::object getObject(BaseContext* ctx, std::string path)
@@ -72,15 +197,32 @@ void initBindingBaseContext(pybind11::module& m)
         .value("SearchParents", BaseContext::SearchDirection::SearchParents)
         .export_values();
 
-    baseContext.def("addObject", &BaseContext::addObject )
-               .def("removeObject", &BaseContext::removeObject)
-               .def("getObject", &getObject)
-               .def("getObjects", &getObjects, pybind11::arg("classInfo"), pybind11::arg("dir") = BaseContext::SearchUp )
-               .def("getDt",&BaseContext::getDt )
-               .def("getTime",&BaseContext::getTime )
+    baseContext.def("addObject", &BaseContext::addObject)
+        .def("removeObject", &BaseContext::removeObject)
+        .def("getObject", &getObject)
+        .def("searchUp", &searchUp, pybind11::arg("className") = std::string(),
+                                    pybind11::arg("templateName") = std::string())
+        .def("searchAllUp", &searchAllUp, pybind11::arg("className") = std::string(),
+                                                  pybind11::arg("templateName") = std::string())
+        .def("searchLocal", &searchLocal, pybind11::arg("className") = std::string(),
+                                          pybind11::arg("templateName") = std::string())
+        .def("searchAllLocal",&searchAllLocal, pybind11::arg("className") = std::string(),
+                                               pybind11::arg("templateName") = std::string())
+        .def("searchParents", &searchParents, pybind11::arg("className") = std::string(),
+                                              pybind11::arg("templateName") = std::string())
+        .def("searchAllParents", &searchAllParents, pybind11::arg("className") = std::string(),
+                                                    pybind11::arg("templateName") = std::string())
+        .def("searchDown", &searchDown, pybind11::arg("className") = std::string(),
+                                        pybind11::arg("templateName") = std::string())
+        .def("searchAllDown", &searchAllDown, pybind11::arg("className") = std::string(),
+                                              pybind11::arg("templateName") = std::string())
+        .def("searchRoot", &searchRoot, pybind11::arg("className") = std::string(),
+                                        pybind11::arg("templateName") = std::string())
+        .def("searchAllRoot", &searchAllRoot, pybind11::arg("className") = std::string(),
+                                              pybind11::arg("templateName") = std::string())
+        .def("getDt",&BaseContext::getDt )
+        .def("getTime",&BaseContext::getTime )
         ;
-
-
 }
 
 }
