99b71aa4228ecdff28d6fd35759b2f37045b54fd
Colin Guyon <colin.guyon@insimo.fr>
2019-10-31 20:00:51 +0100

    [ISSofaPython] CHANGE python_mock/ISSofaPython.py: add some more content to this basic mock
    (cherry picked from commit 3e212201bb16764db2578769e09b50fab7346686)

--- python_mock/ISSofaPython.py
+++ python_mock/ISSofaPython.py
@@ -8,3 +8,15 @@ def loadPlugin(name):
 
 def msg_fatal(msg):
     pass
+
+class _NodeMock(object):
+    pass
+
+def createRootNode(name):
+    return _NodeMock()
+
+def initializeGraph(node):
+    pass
+
+def cleanupGraph(node):
+    pass
