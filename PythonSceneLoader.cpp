548f82f178583bc57d1e181257e4ce456fc83577
Colin Guyon <colin.guyon@insimo.fr>
2019-12-06 15:42:38 +0000

    [ISSofaPython] FIX PythonSceneLoader: prev. scene local modules could be imported instead of ones of the new scene
    We now remove the directory of the previous scene from sys.path
    before loading the new scene.
    Also in case we have for eg. <prev_scene_dir>/some_local_module.py and
    <new_scene_dir>/some_local_module.py we remove 'some_local_module' from
    sys.modules (ie. if it was imported) before loading the new scene, to
    ensure a new `import some_local_module` does not get the already-imported
    local module of the previous scene dir, but loads
    <new_scene_dir>/some_local_module.py as expected.
    
    //Ref// T7851
    
    (cherry picked from commit 4e3fe9d5172a1b22d74a5b0f50271762b4d662f8)

--- PythonSceneLoader.cpp
+++ PythonSceneLoader.cpp
@@ -13,6 +13,7 @@
 #include <dlfcn.h>
 #endif
 #include <pybind11/stl.h> // to cast std::vector<std::string> into list<str>
+#include <pybind11/eval.h>
 
 namespace sofa
 {
@@ -97,10 +98,61 @@ sofa::simulation::Node::SPtr PythonSceneLoader::load(const char *filename, const
     auto node = sofa::simulation::tree::getSimulation()->createNewGraph("root");
     GNode::SPtr gNode = sofa::core::objectmodel::SPtr_static_cast<GNode>(node);
 
+    static std::string prevSceneDir = "";
     try
     {
         pybind11::module sys = pybind11::module::import("sys");
-        sys.attr("path").cast<pybind11::list>().append(fileDir);
+        if (!prevSceneDir.empty())
+        {
+            std::cout << "Cleaning previous scene:" << std::endl;
+
+            // Remove the previous scene directory from sys.path so that new modules imports
+            // are not searched in it
+            std::cout << "  - remove '" << prevSceneDir << "' from sys.path" << std::endl;
+            sys.attr("path").attr("remove")(prevSceneDir);
+
+            // In case the previous and new scenes dirs contain local python modules having
+            // identical names, we delete any imported module residing in the previous scene dir
+            // from sys.modules, to ensure new potential imports of the same modules names do not
+            // return already-imported modules residing next to the previous scene.
+            // We want new imports to load the modules residing next to the new scene.
+            pybind11::object scope = pybind11::globals();
+            scope["PREV_SCENE_DIR"] = prevSceneDir;
+            scope["SCENE_FILE_PATH"] = filename;
+            pybind11::exec(R"(
+                import sys
+                from os import path
+                prev_scene_local_modules = []
+                # SCENE_FILE_NAME = path.basename(SCENE_FILE_PATH)
+                for name, module in sys.modules.iteritems():
+                    module_file_path = getattr(module, '__file__', None)
+                    if module_file_path is None:
+                        continue
+                    # Uncomment the following lines if you want to avoid a reload of the python module
+                    # of the scene when doing a 'reload' from the GUI (reloading is convenient when developing)
+                    # if (module_file_path.endswith(SCENE_FILE_NAME) or
+                    #         module_file_path.endswith(SCENE_FILE_NAME + 'c')):
+                    #     p1 = path.abspath(SCENE_FILE_PATH if not SCENE_FILE_PATH.endswith('c')
+                    #                       else SCENE_FILE_PATH[:-1])
+                    #     p2 = path.abspath(module_file_path if not module_file_path.endswith('c')
+                    #                       else module_file_path[:-1])
+                    #     if p1 == p2:
+                    #         continue  # the scene module was already imported and won't be re-read
+                    if module_file_path.startswith(PREV_SCENE_DIR):
+                        prev_scene_local_modules.append(name)
+                for name in prev_scene_local_modules:
+                    print('  - delete %r from sys.modules' % name)
+                    del sys.modules[name]
+                )",
+                scope
+            );
+        }
+
+        if (!fileDir.empty())
+        {
+            sys.attr("path").cast<pybind11::list>().append(fileDir);
+        }
+        prevSceneDir = fileDir;
 
         sys.attr("argv") = sceneArguments;
 
