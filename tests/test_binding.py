17ae37e9368765d4d3452f9a8c33292a17ac8cff
François Jourdes <francois.jourdes@insimo.fr>
2021-01-27 09:52:17 +0100

    [ISSofaPython][tests] import sofa module in test_binding to have the object factory populated with all what has been compiled (//Ref// T10740)
    (cherry picked from commit 48ef9eb30e34169319e6780d92364a2da0fc2f07)

--- tests/test_binding.py
+++ tests/test_binding.py
@@ -1,11 +1,4 @@
-import ISSofaPython as Sofa
-
-
-for plugin in ('ISSofaPythonPlugin'):
-    try:
-        Sofa.loadPlugin(plugin)
-    except (RuntimeError, ValueError) as err:
-        print('Failed to load the %r plugin: %s', plugin, err)
+import sofa
 
 def createScene(root_node):
     mo_0 = root_node.createObject("MechanicalObject")
@@ -14,8 +7,8 @@ def createScene(root_node):
     child_node = root_node.createChild("child")
     mo_1 = child_node.createObject("MechanicalObject")
     mapping = child_node.createObject("IdentityMapping")
-    print(isinstance(mapping, Sofa.Base))
-    print(isinstance(mapping, Sofa.BaseObject))
+    print(isinstance(mapping, sofa.Base))
+    print(isinstance(mapping, sofa.BaseObject))
     # print(isinstance(mapping, Sofa.BaseMapping))
     print(type(mapping))
     print(mapping.getClass().className)
@@ -26,8 +19,10 @@ def createScene(root_node):
     mo_0.cleanup()
     print("mapping reinit")
 
-    controller = Sofa.PythonController()
+    controller = sofa.PythonController()
     print("controller created")
     print(type(controller))
     child_node.addObject(controller)
 
+if __name__ == "__main__":
+    createScene(sofa.createRootNode("root"))
